f82f58a0fd6263fb1d07fcf8d4d6278f71f547ab
AndereX-dev <andreas.osdal@hotmail.com>
2024-06-25 11:57:45 +0200

    commit changes

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+// let is a keyword in use to command the console to use a variable
+// "greeting"
+let greeting = "Good morning everyone!"
+
+// the console contains the variable "greeting" which shows the greeting
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+let numberA = 20
+let numberB = 30
+
+console.log(numberA)
+console.log(numberB)
+
+let numberC = numberA + numberB
+console.log(numberC)
+
+let playerName = "Ashborn"
+
+//Basic data types
+//Strings (Text)
