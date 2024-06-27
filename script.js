ccea17c2480fe53141140fd457a2ea121b09499c
AndereX-dev <andreas.osdal@hotmail.com>
2024-06-27 12:12:11 +0200

    commit these changes

--- script.js
+++ script.js
@@ -20,3 +20,79 @@ let playerName = "Ashborn"
 
 //Basic data types
 //Strings (Text)
+
+//let isOkay = true
+
+//let myVar = 35
+//const myOtherVar = 230
+
+//let name = "Andy"
+
+//let person = {
+    //name: "Ashborn",
+    //Age: 25
+//}
+
+// let names = [
+    //"Andy",
+    //"Ash",
+    //"Born"
+//]
+
+function greeting () {
+    let userName = "Ashborn"
+    let greeting = "Hello There!" + userName
+
+    console.log(greeting)
+}
+
+greeting ()
+greeting ()
+greeting ()
+
+function buttonHandler () {
+    console.log("Button Clicked!")
+}
+
+let myButton = document.getElementById("btn")
+console.log(myButton)
+myButton.addEventListener("click", buttonHandler)
+
+let counter = document.querySelector("#counter")
+console.log(counter)
+
+let displayElement = counter.querySelector("span")
+console.log(displayElement)
+
+function increment () {
+    let value = Number (displayElement.textContent)
+    let newValue = value + 1
+
+    displayElement.textContent = newValue
+    //console.log("Incrementing")
+}
+
+function decrement () {
+    let value = Number (displayElement.textContent)
+    let newValue = value - 1
+
+    displayElement.textContent = newValue
+    console.log("Decrementing")
+}
+
+function reset () {
+    console.log("Reseting")
+}
+
+let incrementButton = counter.querySelector("button:nth-child(2)")
+let decrementButton = counter.querySelector("button:nth-child(3)")
+let resetButton = counter.querySelector("button:nth-child(4)")
+
+console.log(incrementButton)
+console.log(decrementButton)
+console.log(resetButton)
+
+incrementButton.addEventListener("click", increment)
+decrementButton.addEventListener("click", decrement)
+resetButton.addEventListener("click", reset)
+
